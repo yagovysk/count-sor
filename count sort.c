1a1b4870ff042f51a0524383e7577ffdceb1846e
yagovysk <45441676+yagovysk@users.noreply.github.com>
2018-11-28 23:16:07 -0200

    Add files via upload

---
+++ count sort.c
@@ -0,0 +1,413 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int i, j, x, y, z, a[x], b[y], c[z];
+	z=0;
+	for(z=0;z<9;z++){
+		c[z] = z+1;
+	}
+	i = 0;
+	j = 0;
+	printf("digite 10 valores de 0 a 10, a ordem de sua preferencia. \n");
+	scanf("%i \n %i \n %i \n %i \n %i \n %i \n %i \n %i \n %i \n %i", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
+	if(a[0]==1){
+		a[0] = c[0];
+		printf("inbox 1:%i \n", a[0]);
+	}
+	if(a[1]==1){
+		a[1] = c[0];
+		printf("inbox 1:%i \n", a[1]);	
+	}
+	if(a[2]==1){
+		a[2] = c[0];
+		printf("inbox 1:%i \n", a[2]);
+	}
+	if(a[3]==1){
+		a[3] = c[0];
+	printf("inbox 1:%i \n", a[3]);	
+	}
+	if(a[4]==1){
+		a[4] = c[0];
+		printf("inbox 1:%i \n", a[4]);
+	}	
+	if(a[5]==1){
+		a[5] = c[0];
+		printf("inbox 1:%i \n", a[5]);
+	}
+	if(a[6]==1){
+		a[6] = c[0];
+		printf("inbox 1:%i \n", a[6]);
+	}
+	if(a[7]==1){
+		a[7] = c[0];
+		printf("inbox 1:%i \n", a[7]);
+	}
+	if(a[8]==1){
+		a[8] = c[0];
+		printf("inbox 1:%i \n", a[8]);
+	}
+	if(a[9]==1){
+		a[9] = c[0];
+		printf("inbox 1:%i \n", a[9]);
+	}
+	if(a[0]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[1]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[2]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[3]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[4]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[5]==2){
+	    c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[6]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[7]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[8]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[9]==2){
+		c[1] = 1;
+		printf("inbox 2:%i \n", c[1]);
+	}
+	if(a[0]==3){
+	    c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[1]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[2]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[3]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[4]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[5]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[6]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[7]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[8]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[9]==3){
+		c[2] = 1;
+		printf("inbox 3:%i \n", c[2]);
+	}
+	if(a[0]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[1]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[2]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[3]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[4]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[5]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[6]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[7]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[8]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[9]==4){
+		c[3] = 1;
+		printf("inbox 4:%i \n", c[3]);
+	}
+	if(a[0]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[1]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[2]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[3]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[4]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[5]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[6]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[7]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[8]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[9]==5){
+		c[4] = 1;
+		printf("inbox 5:%i \n", c[4]);
+	}
+	if(a[0]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[1]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[2]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[3]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[4]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[5]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[6]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[7]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[8]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[9]==6){
+		c[5] = 1;
+		printf("inbox 6:%i \n", c[5]);
+	}
+	if(a[0]==7){
+		v[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[1]==7){
+		c[6] = 1; 
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[2]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[3]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[4]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[5]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[6]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[7]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[8]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[9]==7){
+		c[6] = 1;
+		printf("inbox 7:%i \n", c[6]);
+	}
+	if(a[0]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[1]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[2]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[3]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[4]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[5]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[6]==8){
+		c[8] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[7]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[8]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[9]==8){
+		c[7] = 1;
+		printf("inbox 8:%i \n", c[7]);
+	}
+	if(a[0]==9){
+		c[8] = 1;
+		printf("inbox 9:%i \n", c[8]);
+	}
+	if(a[1]==9){
+		c[8] = 1;
+		printf("inbox 9:%i \n", c[8]);
+	}
+	if(a[2]==9){
+		c[8] = 1;
+		printf("inbox 9:%i \n", c[8]);
+	}
+	
+	if(a[0] && a[1] == 1){
+		c[0] = a[0]+a[1];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[2] == 1){
+		c[0] = a[0]+a[2];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[3] == 1){
+		c[0] = a[3]+a[0];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[4] ==1){
+		c[0] = a[4]+a[0];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[0] && a[5] == 1){
+		c[0] = a[5]+a[0];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[0] && a[6] == 1){
+		c[0] = a[6]+a[0];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[0] && a[7] == 1){
+		c[0] = a[7]+a[0];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[8] == 1){
+		c[0] = a[8]+a[0];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[9] == 1){
+		c[0] = a[9]+a[0];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] && a[5] && a[6] && a[7] && a[8] && a[9] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9];
+		printf("inbox 1:%i \n", c[0]);		
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] && a[5] && a[6] && a[7] && a[8] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] && a[5] && a[6] && a[7] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7];
+		printf("inbox 1:%i \n", c[0]);		
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] && a[5] && a[6] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] && a[5] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4]+a[5];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[1] && a[2] && a[3] && a[4] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3]+a[4];
+		printf("inbox 1:%i \n", c[0]);
+	}
+	if(a[0] && a[1] && a[2] && a[3] == 1){
+		c[0] = a[0]+a[1]+a[2]+a[3];
+		printf("inbox 1:%i \n", c[0]);	
+	}
+	if(a[1])
+	return 0;
+}
